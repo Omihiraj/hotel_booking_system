318ba26731f10ce07aed2f3a77701bd5a61d9323
Mihiraj Wijesooriya <omihiraj@gmail.com>
2024-09-14 23:15:13 +0530

    Initialize App

---
+++ lib/main_page.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:hotel_management_app/screens/main/booking_screen.dart';
+import 'package:hotel_management_app/screens/main/discover_screen.dart';
+import 'package:hotel_management_app/screens/main/favourite_screen.dart';
+import 'package:hotel_management_app/screens/main/message_screen.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List<Widget> screenList = const [
+    DiscoverScreen(),
+    FavouriteScreen(),
+    BookingScreen(),
+    MessageScreen(),
+  ];
+
+  int screenNo = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screenList[screenNo],
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Container(
+          width: 400,
+          height: 70,
+          padding: const EdgeInsets.all(15),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              color: const Color.fromARGB(215, 0, 0, 0)),
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                bottomNavigationIcon(
+                  index: 0,
+                  icon: Icons.ac_unit,
+                  navigationText: "Discover",
+                ),
+                bottomNavigationIcon(
+                  index: 1,
+                  icon: Icons.ac_unit,
+                  navigationText: "Favorites",
+                ),
+                bottomNavigationIcon(
+                  index: 2,
+                  icon: Icons.ac_unit,
+                  navigationText: "Bookings",
+                ),
+                bottomNavigationIcon(
+                  index: 3,
+                  icon: Icons.ac_unit,
+                  navigationText: "Messages",
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget bottomNavigationIcon(
+      {required IconData icon,
+      required String navigationText,
+      required int index}) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          screenNo = index;
+        });
+      },
+      child: Column(
+        children: [
+          Icon(
+            icon,
+            color: index == screenNo ? Colors.white : Colors.grey,
+            size: 18,
+          ),
+          Text(
+            navigationText,
+            style: TextStyle(
+                color: index == screenNo ? Colors.white : Colors.grey,
+                fontSize: 12),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/main/discover_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class DiscoverScreen extends StatefulWidget {
+  const DiscoverScreen({super.key});
+
+  @override
+  State<DiscoverScreen> createState() => _DiscoverScreenState();
+}
+
+class _DiscoverScreenState extends State<DiscoverScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Stack(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 250,
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50)),
+                  image: DecorationImage(
+                    image: const NetworkImage(
+                        "https://images.unsplash.com/photo-1445019980597-93fa8acb246c?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+                    fit: BoxFit.cover,
+                    colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.5), BlendMode.darken),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Norway",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Icon(
+                          Icons.person_2_sharp,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      "Hey, Martin! Tell Us Where you want to go",
+                      style: TextStyle(color: Colors.white, fontSize: 24),
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
